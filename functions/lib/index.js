20937f9762b6abfbcf25622e1269fa158842cb51
alireza.zareidowlatabadi <alireza.zareidowlatabadi@gmail.com>
2025-05-28 04:52:50 +0000

    Preparing the project structure based on TypeScript

---
+++ functions/lib/index.js
@@ -0,0 +1,10 @@
+"use strict";
+// توابع واقعی پروژه شما در اینجا اضافه خواهند شد.
+// مثال:
+// import { onRequest } from "firebase-functions/v2/https";
+// import * as logger from "firebase-functions/logger";
+//
+// export const myFunction = onRequest((request, response) => {
+//   // منطق تابع شما
+// });
+//# sourceMappingURL=index.js.map
---
+++ functions/src/index.ts
@@ -0,0 +1,8 @@
+// توابع واقعی پروژه شما در اینجا اضافه خواهند شد.
+// مثال:
+// import { onRequest } from "firebase-functions/v2/https";
+// import * as logger from "firebase-functions/logger";
+//
+// export const myFunction = onRequest((request, response) => {
+//   // منطق تابع شما
+// });
